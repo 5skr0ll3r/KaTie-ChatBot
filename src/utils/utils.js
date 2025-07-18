9e6707be672d57906e0430e46ecd5d4b693f3550
5skr0ll3r <47701859+5skr0ll3r@users.noreply.github.com>
2025-07-18 16:30:53 +0300

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,3 @@
 const removeElements = function(stri){ return stri.replace(/(\<)(.+?)(\>)/g, "<>")}
 
-function randomCase(str) {
-	return str.split('').map(c => Math.random() < 0.5 ? c.toLowerCase() : c.toUpperCase()).join('');
-}
-
 module.exports = { removeElements };
